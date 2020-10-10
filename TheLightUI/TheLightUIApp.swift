7d9fe64a4279f4b3656d9c2c026293a108ec725c
Egor Astakhov <noise718@gmail.com>
2020-10-10 13:54:45 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Егор Астахов on 10.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
